a750e0cab3b0970df497dc2632837cfdf5300e55
cassecou <angelicgoyal1995@gmail.com>
2020-07-12 21:29:32 +0530

    Added nested class wheel to calculate detail for gear inches
    Requirement to calculate Gear inches added which takes details of wheel but wheel is not involved directly with requirements, therefore nested in Gear.
    Now, only ratio or both parameters(ratio, gear inches) can be requested from the comparison calculator.

--- GearDemo.java
+++ GearDemo.java
@@ -1,16 +1,37 @@
 package learningJava;
 class Gear{
-
 	private int chainring, cog;
-	Gear(int chainring, int cog){
+	private Wheel wheel;
+	
+	class Wheel {
+		private double rim, tire;
+		Wheel(double rim, double tire) {
+			this.rim = rim;
+			this.tire = tire;
+		}
+		double getDiameter(){
+			return (rim + (tire*2));
+		}
+	}
+
+	Gear(int chainring, int cog) {
 		this.chainring = chainring;
 		this.cog = cog;
 	}
 
-	double getRatio(){
+	Gear(int chainring, int cog, double rim, double tire){
+		this.chainring = chainring;
+		this.cog = cog;
+		wheel = new Wheel(rim, tire);
+	}
+
+	public double getRatio(){
 		return (double)chainring/cog;
 	}
-	
+
+	public double getGearInches() {
+		return this.getRatio()*wheel.getDiameter();
+	}
 }
 
 public class GearDemo{
@@ -19,5 +40,14 @@ public class GearDemo{
 		Gear gear2 = new Gear(30, 27);
 		System.out.println("Gear 1 ratio : " + gear1.getRatio());
 		System.out.println("Gear 2 ratio : " + gear2.getRatio());
+
+		System.out.println("\n****Compare gear inches now **** \n");
+
+		Gear gear3 = new Gear(55, 11, 1, 1.5);
+		Gear gear4 = new Gear(40, 20, 2, 2.5);
+		System.out.println("            Gear 3 | Gear 4");
+		System.out.println("_____________________________");
+		System.out.println("Ratio        " + gear3.getRatio() + "   | " + gear4.getRatio());
+		System.out.println("Gear Inches  " + gear3.getGearInches() + "  | " + gear4.getGearInches());
 	}
 }
