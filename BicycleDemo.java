11514e97001f0150cd91c9c4f5c37fa6c14d49d8
cassecou <angelicgoyal1995@gmail.com>
2020-07-15 01:02:03 +0530

    Requirment for Mountain Bike
    - Bicycle updated as super class which contains common member variables and functions from subclass
    - RoadBike and MountainBike created as sub classes
    - Similarity of methods and variables, therefore inheritance more suitable than interface or abstract class.

--- BicycleDemo.java
+++ BicycleDemo.java
@@ -2,29 +2,79 @@ package learningJava;
 
 class Bicycle {
 	private char cycleSize;
-	private String tapeColor;
-	private static int chain = 10, tireSize = 23;
+	private static int chain = 10;
+	private double tireSize;
 
-	Bicycle(char cycleSize, String tapeColor){
+	Bicycle(char cycleSize, double tireSize){
 		this.cycleSize = cycleSize;
-		this.tapeColor = tapeColor;
+		this.tireSize = tireSize;
+	}
+
+	Bicycle(){
+		this.cycleSize = 'S';
+		this.tireSize = 2.5;
 	}
 
 	public char getCycleSize() {
 		return cycleSize;
 	}
 
-	public void spares(){
+	protected void spares(){
 		System.out.println("Chain : " + chain);
 		System.out.println("Tire Size : " + tireSize);
+	}
+}
+
+class RoadBike extends Bicycle {
+	private String tapeColor;
+	RoadBike(char cycleSize, double tireSize, String tapeColor) {
+		super(cycleSize, tireSize);
+		this.tapeColor = tapeColor;
+	}
+
+	RoadBike(char cycleSize, String tapeColor) {
+		super(cycleSize, 23);
+		this.tapeColor = tapeColor;
+	}
+
+	public void spares() {
+		super.spares();
 		System.out.println("Tape color : " + tapeColor);
 	}
 }
 
+class MountainBike extends Bicycle {
+	private String frontShock, rearShock;
+	MountainBike(char cycleSize, double tireSize, String frontShock, String rearShock) {
+		super(cycleSize, tireSize);
+		this.frontShock = frontShock;
+		this.rearShock = rearShock;
+	}
+
+	MountainBike(char cycleSize, String frontShock, String rearShock) {
+		super(cycleSize, 2.1);
+		this.frontShock = frontShock;
+		this.rearShock = rearShock;
+	}
+
+	public void spares() {
+		super.spares();
+		System.out.println("Rear-shock : " + rearShock);
+	}
+}
+
+
 public class BicycleDemo {
 	public static void main(String args[]){
-		Bicycle bike = new Bicycle('M', "Red");
-		System.out.println("Cycle size : " + bike.getCycleSize());
-		bike.spares();
+		RoadBike bike1 = new RoadBike('M', "Red");
+		System.out.println("*** Road bike ***");
+		System.out.println("Cycle size : " + bike1.getCycleSize());
+		bike1.spares();
+
+		MountainBike bike2 = new MountainBike('S', "Manitou", "Fox");
+		System.out.println("*** Mountain bike ***");
+		System.out.println("Cycle size : " + bike2.getCycleSize());
+		bike2.spares();
+
 	}
 }
